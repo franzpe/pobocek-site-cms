5370c7f5bc50c326d68d6c9c5f04e991d4cb9b88
František Poboček <pobocekfrantisek@gmail.com>
2023-02-11 14:12:25 +0100

    prettier

--- config/database.ts
+++ config/database.ts
@@ -1,15 +1,11 @@
-import path from "path";
+import path from 'path';
 
 export default ({ env }) => ({
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: env(
-        "DATABASE_FILENAME",
-        path.join(__dirname, "..", "..", ".tmp/data.db")
-      ),
-      // filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
+	connection: {
+		client: 'sqlite',
+		connection: {
+			filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '..', '.tmp/data.db'))
+		},
+		useNullAsDefault: true
+	}
 });
--- config/production/server.ts
+++ config/production/server.ts
@@ -1,9 +1,6 @@
 export default ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-  dirs: {
-    public: "/data/public",
-  },
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
+	url: env('RENDER_EXTERNAL_URL'),
+	dirs: {
+		public: '/data/public'
+	}
 });
